1b044430d210affb7d5559a43723ee34eb3562e4
wilson Neto <wngaspar10@gmail.com>
2022-04-17 14:30:01 -0300

    refactor: descrevendo melhor o cenário de teste

--- src/services/CreateLesson.spec.ts
+++ src/services/CreateLesson.spec.ts
@@ -1,19 +1,21 @@
 import { InMemoryLessonsRepository } from "../../test/repositories/InMemoryLessonsRepository";
 import { CreateLesson } from "./CreateLesson";
 
-test("create lesson", async () => {
-  const inMemoryLessonsRepository = new InMemoryLessonsRepository();
-  const createLesson = new CreateLesson(inMemoryLessonsRepository);
+describe("CreateLesson service", () => {
+  it("should be bale to create a new lesson", async () => {
+    const inMemoryLessonsRepository = new InMemoryLessonsRepository();
+    const createLesson = new CreateLesson(inMemoryLessonsRepository);
 
-  await expect(
-    createLesson.execute({ title: "Nova aula" })
-  ).resolves.not.toThrow();
+    await expect(
+      createLesson.execute({ title: "Nova aula" })
+    ).resolves.not.toThrow();
 
-  expect(inMemoryLessonsRepository.items).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({
-        title: "Nova aula",
-      }),
-    ])
-  );
+    expect(inMemoryLessonsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: "Nova aula",
+        }),
+      ])
+    );
+  });
 });
